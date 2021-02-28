1c71ac31ae7b8b8429b2d41b9c548d5a35936ee7
Nguyễn Văn Quế <67778671+nguyenque@users.noreply.github.com>
2021-02-28 22:12:47 +0700

    finish

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ function lightMode() {
   nav.style.backgroundColor = 'rgba(255 255 255 / 50%)'
   textbox.style.backgroundColor = 'rgba(0 0 0 / 50%)'
   toggleIcon.children[0].textContent = 'Light Mode'
-  toggleIcon.children[1].classList.remove('fa-moon', 'fa-sun')
+  toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun')
   
   imageMode('light')
 }
