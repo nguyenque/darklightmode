a3358126963d1c988465512ba7b95d3bb59a4724
Nguyễn Văn Quế <67778671+nguyenque@users.noreply.github.com>
2021-03-01 09:07:14 +0700

    refactory code

--- script.js
+++ script.js
@@ -4,23 +4,23 @@ function getElement(selection) {
   if (element) return element
   throw new Error(`No such ${element} exists. Please check!`)
 }
-// dark mode
-function darkMode() {
-  nav.style.backgroundColor = 'rgba(0 0 0 / 50%)'
-  textbox.style.backgroundColor = 'rgba(255 255 255 / 50%)'
-  toggleIcon.children[0].textContent = 'Dark Mode'
-  toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon')
-  imageMode('dark')
+const toggleSwitch = getElement("toggleCheckbox")
+const nav = getElement('nav')
+const toggleIcon = getElement('toggle-icon')
+const image1 = getElement('image1')
+const image2 = getElement('image2')
+const image3 = getElement('image3')
+const textbox = getElement('text-box')
+
+function toggleDarkLightMode(isDark) {
+  nav.style.backgroundColor = isDark ? 'rgba(0 0 0 / 50%)' : 'rgba(255 255 255 / 50%)';
+  textbox.style.backgroundColor = isDark ? 'rgba(255 255 255 / 50%)' : 'rgba(0 0 0 / 50%)';
+  toggleIcon.children[0].textContent = isDark ? 'Dark Mode' : 'Light Mode';
+  isDark ? toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon') : toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun');
+  isDark ? imageMode('dark') : imageMode('light')
+
 }
-// light mode
-function lightMode() {
-  nav.style.backgroundColor = 'rgba(255 255 255 / 50%)'
-  textbox.style.backgroundColor = 'rgba(0 0 0 / 50%)'
-  toggleIcon.children[0].textContent = 'Light Mode'
-  toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun')
-  
-  imageMode('light')
-}
+
 // Dark or Light image
 function imageMode(color) {
   image1.src = `./img/undraw_proud_coder_${color}.svg`
@@ -28,33 +28,23 @@ function imageMode(color) {
   image3.src = `./img/undraw_conceptual_idea_${color}.svg`
 }
 
-const toggleSwitch = document.querySelector("input[type='checkbox']")
-const nav = getElement('nav')
-const toggleIcon = getElement('toggle-icon')
-const image1 = getElement('image1')
-const image2 = getElement('image2')
-const image3 = getElement('image3')
-const textbox = getElement('text-box')
 // Switch theme dynamically
 function switchTheme(event) {
-  if (event.target.checked) {
-    document.documentElement.setAttribute('data-theme', 'dark')
-    localStorage.setItem('theme', 'dark')
-    darkMode()
-  } else {
-    document.documentElement.setAttribute('data-theme', 'light')
-    localStorage.setItem('theme', 'light')
-    lightMode()
-  }
+  const checked = event.target.checked
+  checked ? document.documentElement.setAttribute('data-theme', 'dark') : document.documentElement.setAttribute('data-theme', 'light');
+  checked ? localStorage.setItem('theme', 'dark') : localStorage.setItem('theme', 'light')
+  checked ? toggleDarkLightMode(true) : toggleDarkLightMode(false)
+
 }
 // Event Listener
 toggleSwitch.addEventListener('change', switchTheme)
+
 // check local storage for theme
 const currentTheme = localStorage.getItem('theme')
-if(currentTheme) {
+if (currentTheme) {
   document.documentElement.setAttribute('data-theme', currentTheme)
-  if(currentTheme === 'dark') {
+  if (currentTheme === 'dark') {
     toggleSwitch.checked = true
-    darkMode()
-  } 
+    toggleDarkLightMode(true)
+  }
 }
